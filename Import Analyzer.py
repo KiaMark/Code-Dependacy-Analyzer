3021f48cfaa40ecb1e5d164c8be08c276ec844c0
Thomas So <tmso@ucdavis.edu>
2015-08-12 21:53:13 -0700

    Added file crawling at top level

--- Import Analyzer.py
+++ Import Analyzer.py
@@ -1,8 +1,5 @@
 import os
 
-folder = input("Input folder name: ")
-folderOpen(folder)
-
 # for fp in os.listdir(file):
 #    print(fp)
 #    ext = os.path.splitext(fp)[-1].lower()
@@ -10,18 +7,31 @@ folderOpen(folder)
 #        print(fp, "is .framwork!")
 
 def folderOpen(folder):
-    for content in os.listdir(folder):
-        ext = os.path.splitext(content)[-1].lower()
-        print("ext: " + ext)
-        if ext == ".h":
-            print(content, "is .h!")
-            header(content)
-        elif os.path.isfile(content):
-            print(content, "is a file, but not .h!")
+    relativePath = os.path.abspath(os.path.join(os.path.dirname( __file__ ), "..", folder))
+    for content in os.listdir(relativePath):
+        # Check if it is a hidden file (.foo), ignore if it is
+        if content[0] == '.':
+            print("Skipping empty file: " + content)
         else:
-            print(content, "is a folder!")
-            folderOpen(content)
+            # You need to create the full path name before checking
+            fullPath = os.path.join(relativePath, folder, content)
+            ext      = os.path.splitext(content)[-1].lower()
 
+            print("Processing fullpath: " + fullPath)
+
+            if ext == ".h":
+                print(content, "is .h!")
+                header(content)
+            elif os.path.isfile(fullPath):
+                print(content, "is a file, but not .h!")
+            elif os.path.isdir(fullPath):
+                print(content, "is a folder!")
+                folderOpen(content)
+            else:
+                print("hurr durr")
 
 def header(folder):
     print(folder, "in header!")
+
+folder = input("Input folder name: ")
+folderOpen(folder)
