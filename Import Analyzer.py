7dec4998ee2c86d4bb7daa972e79cb486a85045e
KiaMark <kiannamark@yahoo.com>
2015-08-14 18:46:09 -0700

    Added code

--- Import Analyzer.py
+++ Import Analyzer.py
@@ -1,27 +1,35 @@
 import os
 
-folder = input("Input folder name: ")
-
-# for fp in os.listdir(file):
-#    print(fp)
-#    ext = os.path.splitext(fp)[-1].lower()
-#    if ext == ".framework":
-#        print(fp, "is .framwork!")
-
 def folderOpen(folder):
+    relativePath = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", folder))
     for content in os.listdir(folder):
-        ext = os.path.splitext(content)[-1].lower()
-        if ext == ".h":
-            print(content, "is .h!")
-            header(content)
-        elif os.path.isfile(content):
-            print(content, "is a file, but not .h!")
+        # Check if it is a hidden file (.foo), ignore if it is
+        if content[0] == '.':
+            print("Skipping empty file: " + content)
         else:
-            print(content, "is a folder!")
-            folderOpen(content)
+            # You need to create the full path name before checking
+            fullPath = os.path.join(folder, content)
+            ext = os.path.splitext(content)[-1].lower()
 
+            #print("Processing fullpath: " + fullPath)
+
+            if ext == ".h":
+                print(content, "is .h!")
+                header(content)
+            elif ext == ".m":
+                print(content, "is .m!")
+                header(content)
+            elif os.path.isfile(fullPath):
+                print(content, "is a file, but not .h or .m!")
+            elif os.path.isdir(fullPath):
+                print(content, "is a folder!")
+                folderOpen(fullPath)
+            else:
+                print("hurr durr")
+                folderOpen(fullPath)
 
 def header(folder):
     print(folder, "in header!")
 
+folder = input("Input folder name: ")
 folderOpen(folder)
