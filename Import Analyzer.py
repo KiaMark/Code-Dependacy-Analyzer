fe7133f5d57780b5c1b81dc9b797252292bcc7c2
KiaMark <kiannamark@yahoo.com>
2015-08-18 23:52:23 -0700

    Updated Code

--- Import Analyzer.py
+++ Import Analyzer.py
@@ -1,7 +1,8 @@
 import os
+size = 0
 
 def folderOpen(folder):
-    relativePath = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", folder))
+    #relativePath = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", folder))
     for content in os.listdir(folder):
         # Check if it is a hidden file (.foo), ignore if it is
         if content[0] == '.':
@@ -10,26 +11,29 @@ def folderOpen(folder):
             # You need to create the full path name before checking
             fullPath = os.path.join(folder, content)
             ext = os.path.splitext(content)[-1].lower()
-
             #print("Processing fullpath: " + fullPath)
 
             if ext == ".h":
-                print(content, "is .h!")
-                header(content)
+                #print(content, "is .h!")
+                header(fullPath)
             elif ext == ".m":
-                print(content, "is .m!")
-                header(content)
-            elif os.path.isfile(fullPath):
-                print(content, "is a file, but not .h or .m!")
+                #print(content, "is .m!")
+                header(fullPath)
             elif os.path.isdir(fullPath):
-                print(content, "is a folder!")
+                #print(content, "is a folder!")
                 folderOpen(fullPath)
             else:
-                print("hurr durr")
-                folderOpen(fullPath)
+                print(content, "is a file, but not .h or .m!")
 
 def header(folder):
     print(folder, "in header!")
+    file = open(folder, 'r')
+    read = file.readlines()
+    file.close()
+    for line in read:
+        if str("import") in line:
+            importFile = line
+            print(importFile)
 
 folder = input("Input folder name: ")
 folderOpen(folder)
